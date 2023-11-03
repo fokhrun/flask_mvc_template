a5942214480f87564b4a81cba8514a1f9d079f08
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-11-04 00:06:03 +0100

    simplify views

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,12 @@
-"""Routes of the applications to be in the main blueprint"""
-
-
 from flask import render_template, session, redirect, url_for
 from .. import db
+from ..models import User
 from . import main
 from .forms import NameForm
-from ..models import User
 
 
-@main.route("/", methods=["GET", "POST"])
+@main.route('/home', methods=['GET', 'POST'])
 def index():
-    """Home page route"""
-
     form = NameForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
@@ -19,19 +14,16 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             db.session.commit()
-            session["known"] = False
+            session['known'] = False
         else:
-            session["known"] = True
-        session["name"] = form.name.data
-        return redirect(url_for(".index"))  # the endpoint for this route
-
-    return render_template(
-        "index.html",
-        form=form,
-        name=session.get("known", False)
-    )
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
-@main.route('/', methods=["GET"])
+@main.route('/', methods=['GET'])
 def table_reservation():
     return render_template('table_reservation/tables.html')
