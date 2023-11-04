0a14ec79ccb2e10b1de9b8cd4d2774af4fe57b5a
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-11-04 14:11:56 +0100

    add basic app

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import os
+
+from flask_migrate import Migrate
+from app import create_app, db
+from app.models import User, Role
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
