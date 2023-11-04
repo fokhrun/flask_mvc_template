8031a634cf9b91c6503a78adb469fded049d56ca
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-11-04 15:06:25 +0100

    enable password management

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 
 """ Models for the template application"""
 
-from . import db
 from enum import Enum
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
 class Role(db.Model):
@@ -19,7 +20,19 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self) -> str:
         return f"<User: {self.username}>"
@@ -54,6 +67,6 @@ class Reservation(db.Model):
     reservation_time_slot = db.Column(db.Enum(ReservationSlot), nullable=False)
     reservation_date = db.Column(db.Date, nullable=False)
     reservation_status = db.Column(db.Boolean, nullable=False, default=False)
-    
+
     def __repr__(self) -> str:
         return f"<Reservations: Table: {self.table_id}, User: {self.user_id}, Slot: {self.reservation_time_slot.name}, Date: {self.reservation_date}>"
