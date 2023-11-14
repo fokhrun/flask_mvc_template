142406cd284f6f563fd6abd5b1ed3a07dbf5d11a
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-11-14 01:38:52 +0100

    add auth view tests

--- tests/auth/test_auth_client.py
+++ tests/auth/test_auth_client.py
@@ -2,15 +2,12 @@
 
 import unittest
 from app import create_app, db
-from app.models import User, Role
-from flask_migrate import Migrate, upgrade
 
 
-class FlaskLoginClientTestCase(unittest.TestCase):
+class FlaskAuthViewTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app("testing")
         self.app_context = self.app.app_context()
-        self.migrate = Migrate(self.app, db)
         self.app_context.push()
         db.create_all()
         self.client = self.app.test_client(use_cookies=True)
@@ -19,11 +16,6 @@ class FlaskLoginClientTestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
-
-    def test_home_page(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(b"Stranger" in response.data)
 
     def test_register(self):
         # register a new account
--- tests/main/test_views.py
+++ tests/main/test_views.py
@@ -2,6 +2,7 @@
 import unittest
 from datetime import date
 from app.main.views import get_next_month_year
+from app import create_app, db
 
 
 class TestViewUtils(unittest.TestCase):
@@ -10,3 +11,20 @@ class TestViewUtils(unittest.TestCase):
         self.assertEqual(get_next_month_year(date(2020, 12, 1)), (2021, 1))
 
 
+class FlaskIndexViewTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b"Stranger" in response.data)
