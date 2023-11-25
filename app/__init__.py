af3d19e61efef3b3059965f1ef28685581b7f6e3
Fakhrunnahar Rupa <rupa0609@gmail.com>
2023-11-25 14:57:42 +0100

    remove dependency on flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,49 +1,32 @@
-
 from flask import Flask
-from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
 
-
-bootstrap = Bootstrap()
-db = SQLAlchemy()
+mail = Mail()
 moment = Moment()
+db = SQLAlchemy()
+
 login_manager = LoginManager()
-login_manager.login_view = "auth.login"
+login_manager.login_view = 'auth.login'
 
 
-def create_app(config_name="default"):
-    """
-    Application factory function that delay the creation of the application
-    Typically invoked from a script that gives time to set the configuration,
-    and can create multiple application instances.
-    Can be very useful during testing.
-
-    Parameters
-    ----------
-    config_name : str, optional
-        to choose the appropriate configuration, by default "default"
-
-    Returns
-    -------
-    flask.Flask
-        Flask application instance
-    """
-    from .main import main as main_blueprint
-    from .auth import auth as auth_blueprint
-
+def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    app.register_blueprint(main_blueprint)
-
     config[config_name].init_app(app)
 
-    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
     db.init_app(app)
-
     login_manager.init_app(app)
-    app.register_blueprint(auth_blueprint, url_prefix="/auth")
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- tests/main/test_views.py
+++ tests/main/test_views.py
@@ -1,5 +1,9 @@
 
+from unittest.mock import MagicMock, patch
 from tests.utils import FlaskAppTestCase
+from app import main
+from flask import Flask, request
+from flask_login import LoginManager, UserMixin
 
 
 class FlaskIndexViewTestCase(FlaskAppTestCase):
@@ -8,3 +12,41 @@ class FlaskIndexViewTestCase(FlaskAppTestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTrue(b"Stranger" in response.data)
+
+
+
+from flask import Flask, request
+
+class MockReservationForm:
+    def __init__(self, **kwargs):
+        self.reserve_date = kwargs.get("reserve_date")
+        self.slot_reserved_statuses = MagicMock()
+
+    def validate_on_submit(self):
+        return False  # Return True to test submission logic
+
+
+import unittest
+
+class TestTableReservation(FlaskAppTestCase):
+
+
+    def test_table_reservation_view(self):
+        self.app.config["LOGIN_DISABLED"] = True
+        view_path = "app.main.views"
+        with self.app.test_request_context("/reserve"):
+            with patch(f"{view_path}.get_reservation_date") as mock_get_date, \
+                 patch(f"{view_path}.get_reservations") as mock_get_res, \
+                 patch(f"{view_path}.get_slot_information") as mock_get_slot_info, \
+                 patch(f"{view_path}.ReservationForm", MockReservationForm):
+                mock_get_date.return_value = "2023-11-22"
+                mock_get_res.return_value = MagicMock()
+                mock_get_slot_info.return_value = (MagicMock(), MagicMock())
+                response = main.views.table_reservation()
+                
+                print (response.data)
+                self.assertEqual(response.status_code, 200)
+                self.assertIn(b"tables.html", response.data)
+                mock_get_date.assert_called_once_with(request.args.get("for_date"))
+                mock_get_res.assert_called_once_with(reservation_date="2023-11-22", user=self.current_user)
+                mock_get_slot_info.assert_called_once_with(mock_get_res.return_value)
